edbd9b91ba7d54965e37a5ab6d1e99df54f5f476
atmel281-stack <atmel281@gmail.com>
2026-03-09 20:48:16 +0000

    .

--- public/script.js
+++ public/script.js
@@ -103,40 +103,7 @@ console.log(myobj.name, obj3.name);//Shelly, Smith
 
 //TASK 6.1
 
-<!DOCTYPE html>
-<html>
-  <head>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <title>Demo</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  </head>
 
-  <body>
-    <nav>
-        <div class="nav-wrapper teal" style="padding-left: 10px;">
-           <a href="#!" class="brand-logo">Ajax Demo</a>
-        </div>
-    </nav>
-    <main class="row" style="padding: 40px">
-         <table>
-           <thead>
-                <tr>
-                   <th>#</th>
-                   <th>name</th>
-                   <th>Type</th>
-                   <th>website</th>
-                </tr>
-                </thead>
-                  <tbody id="result">
-                                                
-                  </tbody>
-           </table>
-     </main>
-     <script>
-     </script>
-  </body>
-</html>
 
 
 //TASK 6
